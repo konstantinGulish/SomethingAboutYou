f0363f50c744a2e4d17262f5691c03fb04ab2592
konstantinGulish <konstantin_gulish@yahoo.com>
2018-05-31 15:55:00 -0400

    first commit

---
+++ src/SomethingAboutYou.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class SomethingAboutYou {
+    public static void main( String[] args ) {
+        Scanner sc = new Scanner(System.in);
+
+    String firstName;
+    int age;
+    String height;
+    double gpa;
+
+    System.out.print( "What is your first name? " );
+    firstName = sc.next();
+
+    System.out.print( "How old are you? " );
+    age = sc.nextInt();
+
+    System.out.print( "How tall are you? " );
+    height = sc.next();
+
+    System.out.print( "What is your GPA? " );
+    gpa = sc.nextDouble();
+
+    System.out.println("name: " + firstName);
+    System.out.println("age: " + age);
+    System.out.println("height: " + height);
+    System.out.println("GPA: " + gpa);
+
+    }
+}
